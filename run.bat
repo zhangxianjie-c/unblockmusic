0f71cd14f265c62d84282bf720a3f0337a3e65f5
xianjie zhang <xianjie.zhang516@gmail.com>
2025-02-12 11:18:57 +0800

    commit

--- run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Running commands...
-pyinstaller --onefile main.py
-pyinstaller --noconsole --onefile --add-binary "unblockneteasemusic-win-x64.exe;." musicLauncher.py
-
-echo Done.
-pause
